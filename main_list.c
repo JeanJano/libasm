b2280eab21625001dba523418a183944471fec7f
Jean SAUVAGE <jsauvage@e3r5p19.clusters.42paris.fr>
2024-06-19 16:38:58 +0200

    ft_list_push_front

--- main_list.c
+++ main_list.c
@@ -4,34 +4,30 @@ void print_list(t_list *list) {
     t_list *tmp = list;
     while (tmp)
     {
-        printf("%d ", tmp->data);
+        printf("%s ", tmp->data);
         tmp = tmp->next;
     }
     printf("\n");
 }
 
-int main() {
-    t_list *list = malloc(sizeof(t_list));
-    list->data = (void*)5;
-
-    t_list *list2 = malloc(sizeof(t_list));
-    list2->data = (void*)6;
-
-    t_list *list3 = malloc(sizeof(t_list));
-    list3->data = (void*)7;
-
-    list->next = list2;
-    list2->next = list3;
-
-    // print list
-    print_list(list);
-
-    ft_list_push_front(&list, (void*)4);
-    print_list(list);
-
-    // // TEST list_size
+void free_list(t_list *list) {
+    t_list *tmp;
+    while (list)
     {
-        // int i = ft_list_size(list);
-        // printf("%d\n", ft_list_size(list));
+        tmp = list;
+        list = list->next;
+        free(tmp);
     }
+}
+
+int main() {
+    t_list *list = NULL;
+    ft_list_push_front(&list, (void *)"yoyoyo");
+    ft_list_push_front(&list, (void *)"yoyoyo");
+    ft_list_push_front(&list, (void *)"yoyoyo");
+    print_list(list);
+
+    printf("%d\n", ft_list_size(list));
+
+    free_list(list);
 }
--- src/ft_list_push_front.s
+++ src/ft_list_push_front.s
@@ -5,15 +5,19 @@ section .text
     extern malloc
 
 ft_list_push_front:
-    mov QWORD rbx, rdi ; Save the pointer to the list
+    push rsi
+    mov rbx, rdi ; Save the pointer to the list
 
-    mov rdi, 16 ; Allocate memory for the new element
+    mov rdi, 16 ; 2 data members of 8 bytes each
     call malloc wrt ..plt ; Allocate memory for the new element
     cmp rax, 0 ; Check if malloc failed
     je .end ; If it did, jump to the error handling code
 
-    mov QWORD [rax], rsi ; Set the data of the new element
-    mov QWORD [rax + 8], rbx ; Set the next pointer of the new element
+    pop rsi
+    mov [rax], rsi ; Set the data of the new element
+    mov rcx, [rbx] ; dereference the pointer to the list
+    mov [rax + 8], rcx ; Set the next pointer of the new element
+    mov [rbx], rax ; Set the new element as the head of the list
 
 .end:
     ret
